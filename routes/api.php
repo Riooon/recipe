dc4ddab41183d8eaf0709f1279dad179aad6ca3f
riooon <riotanaka.jp@gmail.com>
2020-10-20 22:18:01 +0900

    Created GET API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('recipe','ExportController@recipe');
 Route::get('recipe/{recipe}','ExportController@recipe_item');
+
+Route::get('courses','ExportController@courses');
+Route::get('course/{course}','ExportController@course');
